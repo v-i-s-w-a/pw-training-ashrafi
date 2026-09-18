ee0c48fb253938f655b1ebd56107a05afc484212
Ashrafi Firdos <firdosashrafi95@gmail.com>
2026-09-18 20:15:16 +0530

    test: add cartPage fixture

--- tests/cart-page.spec.ts
+++ tests/cart-page.spec.ts
@@ -1,32 +1,9 @@
-import { test, expect } from '@playwright/test';
-import { CartPage } from '../pages/cart-page';
+import { test, expect } from './fixtures';
 
-test('Cart page should display and remove products', async ({ page }) => {
+test('Cart page should display and remove products', async ({ cartPage }) => {
 
-    // Login
-    await page.goto('https://www.saucedemo.com/');
-
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
-
-    // Add two products
-    const product1 = 'Sauce Labs Bolt T-Shirt';
-    const product2 = 'Sauce Labs Fleece Jacket';
-
-    await page.locator('[data-test="inventory-item"]')
-        .filter({ hasText: product1 })
-        .getByRole('button', { name: 'Add to cart' })
-        .click();
-
-    await page.locator('[data-test="inventory-item"]')
-        .filter({ hasText: product2 })
-        .getByRole('button', { name: 'Add to cart' })
-        .click();
-
-    // Open cart
-    const cartPage = new CartPage(page);
-    await cartPage.open();
+    const product1 = 'Sauce Labs Backpack';
+    const product2 = 'Sauce Labs Bike Light';
 
     // Assert both products are in the cart
     const itemNames = await cartPage.itemNames();
